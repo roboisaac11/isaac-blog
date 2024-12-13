6d7b601b8cc2237e1333e6e9b94b7ab0804d606a
Isaac Kroeker <isaackroeker@gmail.com>
2024-12-13 17:34:40 -0600

    update posts

--- stuff.ps1
+++ stuff.ps1
@@ -2,10 +2,14 @@
 robocopy "C:\Users\isaac\OneDrive\Documents\Obsidian Vault\isaac-blog" "C:\Users\isaac\OneDrive\Coding\isaacblog\content\posts" /mir
 
 # Build
-hugo
+hugo -b "/isaac-blog/"
 
 # Update images:
 python images.py
 
-echo "Running server"
-hugo server -t terminal
+# push to github
+git add .
+git commit -m "update posts"
+git push origin master
+git subtree split --prefix public -b release
+git push origin release --force
